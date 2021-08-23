a509bcd7158c32d5cbe5caffb74a3a55edce1c91
ibmgeorge <61768333+ibmgeorge@users.noreply.github.com>
2021-08-24 06:42:42 +1000

    update instructions

--- wildfire-labs.js
+++ wildfire-labs.js
@@ -31,12 +31,18 @@ function getLab(email) {
             index = i;
     var table_data = '<div class="alert alert-warning" role="alert">Email not found.</div>'
     if (index != -1) {
-        table_data = '<dl class="row">';
-        for (var i = 1; i < lines[index].length - 2; i++) {
-            table_data += '<dt class="col-sm-4">' + lines[0][i] + '</dt>';
-            table_data += '<dd class="col-sm-8">' + lines[index][i] + '</dd>';
-        }
-        table_data += '</dl>';
+        table_data = '<p>Below is the information you will need to access the exercises:</p>';
+        table_data += '<h5>Using a web browser</h5>';
+        table_data += '<p>Enter this URL address in the browser’s URL area: <mark>'+lines[index][1]+'</mark></p>';
+        table_data += '<p>Authentication with username: <mark>'+lines[index][2]+'</mark></p>';
+        table_data += '<p>using password: <mark>'+lines[index][5]+'</mark></p>';
+        table_data += '<h5>Or using a Remote Desktop Connection:</h5>';
+        table_data += '<p>Enter in the Computer area this IP address: <mark>'+lines[index][3]+'</mark></p>';
+        table_data += '<p>Follow the instructions in the PDF above and authenticate with username: <mark>'+lines[index][4]+'</mark></p>';
+        table_data += '<p>using the same password: <mark>'+lines[index][5]+'</mark></p>';
+        table_data += '<p><small>When the password for USER1 is required, enter <mark>USER1</mark>  (USER1 is a RACF identity)</small></p>';
+        table_data += '<p><small>When the password for Fred is required, enter <mark>fredpwd</mark> (Fred is not a RACF identity and case matters)</small></p>';
+        table_data += '<p><strong>Please DO NOT shut Windows down, just close the session or disconnect from the remote desktop.</strong></p>';
     }
     $('#lab').html(table_data);
 }
